5460b75aeadcee321bdf18751d361b046816012a
Zach <itsaboy3366@gmail.com>
2023-09-04 19:02:49 -0400

    added error handling and modal

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -8,6 +8,20 @@ $().ready(() => {
         });
     };
 
+    // Button that clears the error modal
+    const clearError = () => {
+        $("#error-button").on("click", () => {
+            $("#error-modal").removeClass("show");
+            $("#error-modal").addClass("hide");
+            $("#header").removeClass("restrict");
+            $("#main").removeClass("restrict");
+            $("#footer").removeClass("restrict");
+            $("#zip-input").prop("disabled", false);
+            $("#submit-button").prop("disabled", false);   
+        });
+    };
+
     // Function calls
     submitButton();
+    clearError();
 });
