7b667763cc42954274668cde5ac13415466aad9c
itsaboy <itsaboy3366@gmail.com>
2023-09-07 22:11:02 -0400

    updated src references for plant images

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,274 +1,274 @@
 // Zone 1
 const tomato = {
     name: "Tomato",
-    image: "./assets/images/tomatoes.jpg"
+    image: "../assets/images/tomatoes.jpg"
 }
 
 const beans = {
     name: "Beans",
-    image: "./assets/images/beans.jpg"
+    image: "../assets/images/beans.jpg"
 }
 
 const sunflower = {
     name: "Sunflowers",
-    image: "./assets/images/sunflower.jpg"
+    image: "../assets/images/sunflower.jpg"
 }
 
 const lilyOfTheValley = {
     name: "Lily of the Valley",
-    image: "./assets/images/LilyofValley.jpg"
+    image: "../assets/images/LilyofValley.jpg"
 }
 
 //Zone 2
 const carrots = {
     name: "Carrots",
-    image: "./assets/images/carrot.jpg"
+    image: "../assets/images/carrot.jpg"
 }
 
 const onions = {
     name: "Onions",
-    image: "./assets/images/onion.jpg"
+    image: "../assets/images/onion.jpg"
 }
 
 const poppies = {
     name: "Poppies",
-    image: "./assets/images/poppies.jpg"
+    image: "../assets/images/poppies.jpg"
 }
 
 const juniper = {
     name: "Juniper",
-    image: "./assets/images/Juniper.jpg"
+    image: "../assets/images/Juniper.jpg"
 }
 
 //Zone 3
 const asparagus = {
     name: "Asparagus",
-    image: "./assets/images/asparagus.jpg"
+    image: "../assets/images/asparagus.jpg"
 }
 
 const cucumbers = {
     name: "Cucumbers",
-    image: "./assets/images/cucumber.jpg"
+    image: "../assets/images/cucumber.jpg"
 }
 
 const garlic = {
     name: "Garlic",
-    image: "./assets/images/garlic.jpg"
+    image: "../assets/images/garlic.jpg"
 }
 
 const aster = {
     name: "Aster",
-    image: "./assets/images/aster.jpg"
+    image: "../assets/images/aster.jpg"
 }
 
 //Zone 4
 const eggplant = {
     name: "Eggplant",
-    image: "./assets/images/eggplant.png"
+    image: "../assets/images/eggplant.png"
 }
 
 const pumpkin = {
     name: "Pumpkin",
-    image: "./assets/images/pumpkin.jpg"
+    image: "../assets/images/pumpkin.jpg"
 }
 
 const iris = {
     name: "Iris",
-    image: "./assets/images/iris.jpg"
+    image: "../assets/images/iris.jpg"
 }
 
 const daylily = {
     name: "Daylily",
-    image: "./assets/images/daylily.jpg"
+    image: "../assets/images/daylily.jpg"
 }
 
 //Zone 5
 const radishes = {
     name: "Radishes",
-    image: "./assets/images/radishes.jpg"
+    image: "../assets/images/radishes.jpg"
 }
 
 const spinach = {
     name: "Spinach",
-    image: "./assets/images/spinach.jpg"
+    image: "../assets/images/spinach.jpg"
 }
 
 const apples = {
     name: "Apples",
-    image: "./assets/images/apples.jpg"
+    image: "../assets/images/apples.jpg"
 }
 
 const lavender = {
     name: "Lavender",
-    image: "./assets/images/lavender.jpg"
+    image: "../assets/images/lavender.jpg"
 }
 
 //Zone 6
 const squash = {
     name: "Squash",
-    image: "./assets/images/squash.jpg"
+    image: "../assets/images/squash.jpg"
 }
 
 const lettuce = {
     name: "Lettuce",
-    image: "./assets/images/lettuce.jpg"
+    image: "../assets/images/lettuce.jpg"
 }
 
 const oregano = {
     name: "Oregano",
-    image: "./assets/images/oregano.jpg"
+    image: "../assets/images/oregano.jpg"
 }
 
 const coriander = {
     name: "Coriander",
-    image: "./assets/images/coriander.jpg"
+    image: "../assets/images/coriander.jpg"
 }
 
 //Zone 7
 const turnips = {
     name: "Turnips",
-    image: "./assets/images/turnip.jpg"
+    image: "../assets/images/turnip.jpg"
 }
 
 const arugula = {
     name: "Arugula",
-    image: "./assets/images/arugula.jpg"
+    image: "../assets/images/arugula.jpg"
 }
 
 const peony = {
     name: "Peony",
-    image: "./assets/images/peony.jpg"
+    image: "../assets/images/peony.jpg"
 }
 
 const forgetMeNots = {
     name: "Forget-me-nots",
-    image: "./assets/images/forget-me-not.jpg"
+    image: "../assets/images/forget-me-not.jpg"
 }
 
 //Zone 8
 const watermelon = {
     name: "Watermelon",
-    image: "./assets/images/watermelon.jpg"
+    image: "../assets/images/watermelon.jpg"
 }
 
 const okra = {
     name: "Okra",
-    image: "./assets/images/okra.jpg"
+    image: "../assets/images/okra.jpg"
 }
 
 const lantana = {
     name: "Lantana",
-    image: "./assets/images/lantana.jpg"
+    image: "../assets/images/lantana.jpg"
 }
 
 const sage = {
     name: "Sage",
-    image: "./assets/images/sage.jpg"
+    image: "../assets/images/sage.jpg"
 }
 
 //Zone 9
 const broccoli = {
     name: "Broccoli",
-    image: "./assets/images/broccoli.jpg"
+    image: "../assets/images/broccoli.jpg"
 }
 
 const avocado = {
     name: "Avocado",
-    image: "./assets/images/acocado.jpg"
+    image: "../assets/images/acocado.jpg"
 }
 
 const oranges = {
     name: "Oranges",
-    image: "./assets/images/oranges.jpg"
+    image: "../assets/images/oranges.jpg"
 }
 
 const zinnias = {
     name: "Zinnias",
-    image: "./assets/images/zinnias.jpg"
+    image: "../assets/images/zinnias.jpg"
 }
 
 //Zone 10
 const peanuts = {
     name: "Peanuts",
-    image: "./assets/images/peanuts.jpg"
+    image: "../assets/images/peanuts.jpg"
 }
 
 const ginger = {
     name: "Ginger",
-    image: "./assets/images/Ginger.jpg"
+    image: "../assets/images/Ginger.jpg"
 }
 
 const agave = {
     name: "Agave",
-    image: "./assets/images/agave.jpg"
+    image: "../assets/images/agave.jpg"
 }
 
 const geraniums = {
     name: "Geraniums",
-    image: "./assets/images/geraniums.jpg"
+    image: "../assets/images/geraniums.jpg"
 }
 
 //Zone 11
 const beets = {
     name: "Beets",
-    image: "./assets/images/beets.jpg"
+    image: "../assets/images/beets.jpg"
 }
 
 const mangos = {
     name: "Mangos",
-    image: "./assets/images/mango.jpg"
+    image: "../assets/images/mango.jpg"
 }
 
 const begonias = {
     name: "Begonias",
-    image: "./assets/images/begonia.jpg"
+    image: "../assets/images/begonia.jpg"
 }
 
 const chives = {
     name: "Chives",
-    image: "./assets/images/chives.jpg"
+    image: "../assets/images/chives.jpg"
 }
 
 //Zone 12
 const summerSquash = {
     name: "Summer Squash",
-    image: "./assets/images/summer-squash.jpg"
+    image: "../assets/images/summer-squash.jpg"
 }
 
 const peppers = {
     name: "Peppers",
-    image: "./assets/images/peppers.jpg"
+    image: "../assets/images/peppers.jpg"
 }
 
 const borage = {
     name: "Borage",
-    image: "./assets/images/borage.jpg"
+    image: "../assets/images/borage.jpg"
 }
 
 const heliconia = {
     name: "Heliconia",
-    image: "./assets/images/Heliconia.jpg"
+    image: "../assets/images/Heliconia.jpg"
 }
 
 //Zone 13
 const africanBreadfruit = {
     name: "African Breadfruit",
-    image: "./assets/images/african-breadfruit.jpg"
+    image: "../assets/images/african-breadfruit.jpg"
 }
 
 const amazonTreeGrape = {
     name: "Amazon Tree Grape",
-    image: "./assets/images/amazon-tree-grape.jpg"
+    image: "../assets/images/amazon-tree-grape.jpg"
 }
 
 const bushBeans = {
     name: "Bush Beans",
-    image: "./assets/images/bush-beans.jpg"
+    image: "../assets/images/bush-beans.jpg"
 }
 
 const rosemary = {
     name: "Rosemary",
-    image: "./assets/images/rosemary.jpg"
+    image: "../assets/images/rosemary.jpg"
 }
 
 // ^^^ Use dictionary ?? ^^^ //
